a59d0b5740bcd42da40639d59014da8a1ba09026
Maseudimo <muhammedalmaseudi99@gmail.com>
2021-10-18 13:31:55 +0300

    created pokemonlist array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-alert('Hello world');
+let pokemonRepository = (function() {
 
-
-let favoriteFood = 'Bolognese';
-document.write(favoriteFood);
+  let pokemonList = [
+    {name:'Bulbasaur', height:0.7, type:['grass','poison']},
+    {name:"Pidgeotto", height:1.1, type:['flying','normal']},
+    {name:"Blastoise", height:1.6, type:['water',]},
+    {name:"Fearow", height:1.2, type:['flying',"normal"]},
+    ];
