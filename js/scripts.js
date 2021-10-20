8e80543702d4dfdc53eb45223cd30b09ebcce82b
Maseudimo <muhammedalmaseudi99@gmail.com>
2021-10-20 15:36:30 +0300

    loop attempted, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-let pokemonRepository = (function() {
 
-  let pokemonList = [
-    {name:'Bulbasaur', height:0.7, type:['grass','poison']},
-    {name:"Pidgeotto", height:1.1, type:['flying','normal']},
-    {name:"Blastoise", height:1.6, type:['water',]},
-    {name:"Fearow", height:1.2, type:['flying',"normal"]},
-    ];
-    }
-    )
+let pokemonList = [];
+let pokemon1 = {name: "Squirtle", height: 0.5, weight: 9, types: ['water']};
+let pokemon2 = {name: "Beedrill", height: 1, weight: 29.5, types: ['bug','poison']};
+let pokemon3 = {name: "Nidoran", height: 0.5, weight: 9, types: ['poison']};
+let pokemon4 = {name: "Venomoth", height: 1.5, weight: 12.5, types: ['bug','poison']};
+let pokemon5 = {name: "Meganium", height: 1.8, weight: 100.5, types: ['grass']};
+
+
+console.log(pokemonList);
+// to display all the Pokemon names
+for(let i=0; i<pokemonList.length; i++){
+  document.write("<p>" + pokemonList[i].name + "</p>");
+}
