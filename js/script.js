0599f22b968de306c9b792a4f63ab2f3186ee0f4
Maseudimo <muhammedalmaseudi99@gmail.com>
2021-11-04 12:03:31 +0200

    added forEach() loop, IIFE function. add/Get all

--- js/script.js
+++ js/script.js
@@ -1,17 +1,58 @@
-
-
+//List of pokemon Array (IIFE Function)
+let pokemonRepository = (function () {
+// Array of Pokemons, with their names, heights and types
   pokemonList = [
- {name:"Zekrom", height:2.9, weight:345, types:["Dragon","Electric"]},
- {name:"Gyrados",height:6.5, weight:235, types:["Water","Flying"]},
- {name:"Steelix",height:9.2, weight:400, types:["Steel","Ground"]},
- {name:"Houndour",height:0.6,weight:10.8,types:["Dark","Fire"]},
- {name:"Nidoking",height:1.4,weight:62,  types:["Ground","Poison"]}];
+ {
+   name:'Zekrom',
+   height:2.9,
+   weight:345,
+   types:["Dragon","Electric"]},
 
- //Listing the name and height of each pokemon in the array.
- //Listing the name and height of each pokemon in the array.
- for (let i = 0; i < pokemonList.length; i++){
-   document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height +"m");
- // Conditional to deteremine which Pokemon have a height greater than 2.5m. using <br> temporarily until i figure out how to make it a list!
-   if (pokemonList[i].height > 2.5) document.write("  - Wow, thats tall!" + "<br>");
-   else document.write("<br>");
+ {
+
+   name:"Gyrados",
+   height:6.5,
+   weight:235,
+   types:["Water","Flying"]},
+
+ {
+   name:"Steelix",
+   height:9.2,
+   weight:400,
+   types:["Steel","Ground"]},
+
+ {
+   name:"Houndour",
+   height:0.6,
+   weight:10.8,
+   types:["Dark","Fire"]},
+
+ {
+   name:"Nidoking",
+   height:1.4,
+   weight:62,
+   types:["Ground","Poison"]
  }
+   ];
+
+
+   //adds pokemon to the pokedex
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+    return pokemonList
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  }
+
+  })();
+
+// for each function to write pokemon and its name/height/type
+  pokemonRepository.getAll().forEach(function(pokemon){
+  document.write( '<p class="whole-info">' + '<h2 class="pokemon-name">' + pokemon.name + '</h2>' + ' (Height: ' + pokemon.height + '\), ' + ' Types: ' + pokemon.types + '</p> <br>');
+});
