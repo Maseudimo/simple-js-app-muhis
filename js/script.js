1737d6b8d7bdcb47363821f8c6d9f9692714c48e
Maseudimo <muhammedalmaseudi99@gmail.com>
2021-11-12 16:32:36 +0200

    exercise 1.7 added polyfill Used Ajax to send and fetch data asynchronously from an external API
    Used Ajax to send and fetch data asynchronously from an external API

--- js/script.js
+++ js/script.js
@@ -1,61 +1,83 @@
-var pokemonRepository = (function () {
-  var pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
- {
-   name:'Zekrom',
-   height:2.9,
-   weight:345,
-   types:["Dragon","Electric"]},
-
- {
-
-   name:"Gyrados",
-   height:6.5,
-   weight:235,
-   types:["Water","Flying"]},
-
- {
-   name:"Steelix",
-   height:9.2,
-   weight:400,
-   types:["Steel","Ground"]},
-
-
-
-];
-   //adds pokemon to the pokedex
-   function getAll () {
-   return pokemonList;
-}
-function add(pokemon) {
-   pokemonList.push(pokemon);
-}
-
- function addListItem(pokemon) {
-    let listContainer = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('poke-button');
-    listItem.appendChild(button);
-    listContainer.appendChild(listItem);
-    
-        button.addEventListener('click', function() {
-            showDetails(pokemon)
-        });
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("poke-button");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function(event) {
+      showDetails(pokemon);
+    });
+  }
 
- function showDetails(pokemon) {
-     console.log (pokemon);
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
- }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
- return {
-     getAll: getAll,
-     add: add,
-     addListItem: addListItem
- };
- } )();
- pokemonRepository.getAll().forEach(function (pokemon) {
-     pokemonRepository.addListItem(pokemon);
- });
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
+
+
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
